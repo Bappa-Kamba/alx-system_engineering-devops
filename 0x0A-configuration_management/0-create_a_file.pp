b5835c0363da75bae39499cfb435f7bebf1cf347
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-11-24 22:27:30 +0100

    Puppet is wonderful

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Using puppet, create a file with below specifications
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
