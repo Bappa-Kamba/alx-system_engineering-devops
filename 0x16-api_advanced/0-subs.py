556e2d480d298423e3a336d2cb5e5a7f1b16c0e3
Attahiru Kamba <bappahmuhammad18@gmail.com>
2024-05-09 01:43:44 +0100

    correction 5

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,15 +11,12 @@ def number_of_subscribers(subreddit):
         return 0 if invalid subreddit given
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
     # get user agent
     # https://stackoverflow.com/questions/10606133/ -->
     # sending-user-agent-using-requests-library-in-python
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    subscribers = 0
     r = requests.get(url, headers=headers, allow_redirects=False)
-    subscribers = r.json().get('data', {}).get('subscribers')
-    if not subscribers or r.raise_for_status():
-        return 0
+    if r.status_code == 200:
+        subscribers = r.json().get('data', {}).get('subscribers')
     return subscribers
